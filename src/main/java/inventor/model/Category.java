8920065884303ab65fc3be98ab5ec2ccc483ced4
DKichukov <dzhamal.kichukov@gmail.com>
2023-01-22 23:27:01 +0200

    Add/Edit Brands on the Web is done!

--- src/main/java/inventor/model/Category.java
+++ src/main/java/inventor/model/Category.java
@@ -7,8 +7,19 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(length = 45,nullable = false,unique = true)
+    @Column(length = 45, nullable = false, unique = true)
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
 
     public Category() {
     }
@@ -35,5 +46,10 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
